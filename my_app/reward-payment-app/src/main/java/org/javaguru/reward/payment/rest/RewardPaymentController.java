8dc08495c70af5bed02048419180204a582954b6
timonin01 <atimonin2006@gmail.com>
2025-07-13 17:17:02 +0300

    create unique index for employeeId and rewardId and added check in controller

--- my_app/reward-payment-app/src/main/java/org/javaguru/reward/payment/rest/RewardPaymentController.java
+++ my_app/reward-payment-app/src/main/java/org/javaguru/reward/payment/rest/RewardPaymentController.java
@@ -4,12 +4,16 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.javaguru.reward.payment.service.RewardPaymentService;
+import org.javaguru.reward.payment.service.domain.Payment;
 import org.javaguru.reward.payment.service.domain.PaymentStatus;
+import org.javaguru.reward.payment.service.repositories.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RewardPaymentController {
 
     private final RewardPaymentService rewardPaymentService;
+    private final PaymentRepository paymentRepository;
+
 
     @PostMapping(path = "/",
             consumes = "application/json",
@@ -26,11 +32,19 @@ public class RewardPaymentController {
         try {
             rewardPaymentService.pay(request.getEmployeeId(), request.getRewardId(), request.getAmount());
         } catch (Throwable e) {
+            if(!checkPaymentInDB(request.getEmployeeId(), request.getRewardId())){
+                return new RewardPaymentResponse(PaymentStatus.SUCCESS.name());
+            }
             log.info("Send RewardPaymentResponse with PaymentStatus.FAIL");
             return new RewardPaymentResponse(PaymentStatus.FAIL.name());
         }
         log.info("Send RewardPaymentResponse with PaymentStatus.SUCCESS");
         return new RewardPaymentResponse(PaymentStatus.SUCCESS.name());
+    }
+
+    private boolean checkPaymentInDB(Long employeeId,Long rewardId){
+        List<Payment> payments = paymentRepository.findByEmployeeIdAndRewardId(employeeId,rewardId);
+        return payments.size() == 1;
     }
 
 }
